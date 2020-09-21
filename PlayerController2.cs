b46b72ec99b9896dcd15d06ae509ba559dff19b9
kmeawad <71085124+kmeawad@users.noreply.github.com>
2020-09-21 17:31:02 -0400

    Add files via upload

---
+++ PlayerController2.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    public float downForce;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+    private bool isGrounded = false;
+    private bool isRight = true;
+
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatisgrounded;
+
+    private int extraJumps;
+    public int extraJumpValue = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate() {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatisgrounded);
+
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (moveInput > 0 && isRight == false) Flip();
+        if (moveInput < 0 && isRight == true) Flip();
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        if (isGrounded == true)
+        {
+            extraJumps = extraJumpValue;
+        }
+
+        // Use extra jumps
+        if (Input.GetKeyDown(KeyCode.W) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+
+        // Move up when on ground
+        if (Input.GetKeyDown(KeyCode.W) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        // Move down when in air
+        if (Input.GetKeyDown(KeyCode.S)  && isGrounded == false)
+        {
+            rb.velocity = Vector2.down * downForce;
+        }
+
+        
+    }
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        isRight = !isRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Unity.Mathematics.math;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject asteroidPrefab;
+    public float respawnTime = 1f;
+    public float xPos = 22f;
+    public float yPos = 22f;
+  
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        StartCoroutine(asteroidWave());
+    }
+    private void spawnEnemy()
+    {
+        GameObject a = Instantiate(asteroidPrefab) as GameObject;
+        a.transform.position = new Vector2(Mathf.Round(Random.Range(-xPos , xPos)), Mathf.Round(Random.Range(-yPos, yPos)));
+    }
+    IEnumerator asteroidWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnEnemy();
+        }
+    }
+}
